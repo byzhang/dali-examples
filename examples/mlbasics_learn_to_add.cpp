c51cb25e93d020f00f0b15503e97e35de59c5ef1
Jonathan Raiman <jonathanraiman@gmail.com>
2016-05-22 20:14:47 -0700

    switch to using a vector for W + print contextual information

--- examples/mlbasics_learn_to_add.cpp
+++ examples/mlbasics_learn_to_add.cpp
@@ -11,7 +11,6 @@ int main( int argc, char* argv[]) {
     "RNN Kindergarden - Lesson 1 - Learning to add.");
     GFLAGS_NAMESPACE::ParseCommandLineFlags(&argc, &argv, true);
     utils::update_device(FLAGS_device);
-
     // How many examples to put in dataset
     const int NUM_EXAMPLES = 100;
     // How many number to add.
@@ -20,38 +19,31 @@ int main( int argc, char* argv[]) {
     const int ITERATIONS = 150;
     // What is the learning rate.
     double LR = 0.01;
-
     // Generate random examples, all the rows sum to number between 0 and 1.
     auto X = Tensor::uniform(0.0, 1.0 / EXAMPLE_SIZE, {NUM_EXAMPLES, EXAMPLE_SIZE});
     // this is our dataset - please do not
     // consider during backpropagation.
     X = tensor_ops::consider_constant(X);
-
     // Compute sums of elements for each example. This is what we would
     // like the network to output.
     auto Y = X.sum(1);
     Y = tensor_ops::consider_constant(Y);
-
     // Those are our parameters: y_output = W1*X1 + ... + Wn*Xn
     // We initialize them to random numbers between 0 and 1.
-    auto W = Tensor::uniform(0.0, 1.0, {EXAMPLE_SIZE, 1});
-
+    auto W = Tensor::uniform(0.0, 1.0, {EXAMPLE_SIZE});
+    std::cout << "initial weights:" << std::endl;
     W.print();
-
     std::vector<Tensor> params = { W };
     auto solver = solver::SGD(params);
 
     for (int i = 0; i < ITERATIONS; ++i) {
         // What the network predicts the output will be.
-        auto predY = X.dot(W).reshape({NUM_EXAMPLES});
-
+        auto predY = X.dot(W);
         // Squared error between desired and actual output
         // E = sum((Ypred-Y)^2)
         auto error = ( (predY - Y) ^ 2 ).sum();
         // Mark error as what we compute error with respect to.
         error.grad();
-        // Print error so that we know our progress.
-        error.print();
         // Perform backpropagation algorithm.
         graph::backward();
         // Use gradient descent to update network parameters.
@@ -59,8 +51,10 @@ int main( int argc, char* argv[]) {
         // provide a solver class, so that you
         // never how to do it on your own!
         solver.step(params, LR);
+        // Print error so that we know our progress.
+        std::cout << "epoch " << i << ", loss: " << (double)error.w(0) << std::endl;
     }
     // Print the weights after we are done. The should all be close to one.
+    std::cout << "learnt weights:" << std::endl;
     W.print();
-    W.dw.print();
 }
